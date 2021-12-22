023324755f06863421a508f9bb9ce210651eab17
aaronespinoza <aarone@berkeley.edu>
2021-12-21 21:38:12 -0800

    added schema and seeds sql with server starter code

---
+++ db/seeds.sql
@@ -0,0 +1,17 @@
+INSERT INTO department (name)
+VALUES("Curator"),
+      ("Legal"),
+      ("Public Relations"),
+      ("Talent");
+
+INSERT INTO role (title, salary)
+VALUES("Art Dealer", 96000),
+      ("Lawyer", 120000),
+      ("Executive", 150000),
+      ("Writer", 80000);
+
+INSERT INTO employee (first_name, last_name)
+VALUES("Charlotte","York"),
+      ("Miranda","Hobbes"),
+      ("Samantha","Jones"),
+      ("Carrie","Bradshaw");
--- index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const mysql = require("mysql2")
-
-//Connect with SQL
-const db = mysql.createConnection({
-    
-})
